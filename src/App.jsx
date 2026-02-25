bd286f5e01d45660a4608237664ad03d9e01a8b9
wenrou <wenroujunz@gmail.com>
2026-02-25 11:28:29 +0800

    fix: restore scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Home';
 import PrivacyPolicy from './PrivacyPolicy';
 import Support from './Support';
 import RefundPolicy from './RefundPolicy';
 import TermsOfService from './TermsOfService';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<PrivacyPolicy />} />
